e2d1799febf50f59a0fbee4768a73a22bbe47b6c
Keri Moyle <k.moyle@auckland.ac.nz>
2020-12-03 10:32:32 +1300

    Test that an element has a classes attributes before attempting to split it
    Avoids the "TypeError: Cannot read property 'split' of null" error.

--- src/mixins/BaseDiv.js
+++ src/mixins/BaseDiv.js
@@ -7,6 +7,9 @@ export const baseDiv = {
 
   computed: {
     classes() {
+      if (!this.element.hasAttribute('classes')) {
+        return []
+      }
       return [
         this.$options.name.toLowerCase(),
         ...this.element.getAttribute('classes').split(' '),

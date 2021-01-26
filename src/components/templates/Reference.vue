13b4f7e9cd040035161aee89d927b8cf103b0321
Keri Moyle <k.moyle@auckland.ac.nz>
2021-01-26 15:27:54 +1300

    Add blank target to external links

--- src/components/templates/Reference.vue
+++ src/components/templates/Reference.vue
@@ -2,7 +2,7 @@
   <router-link v-if="isInternalReference()" :to="routeDescription">
     {{ element.textContent }}
   </router-link>
-  <a v-else :href="element.getAttribute('refuri')">
+  <a v-else :href="element.getAttribute('refuri')" target="_blank">
     {{ element.textContent }}
   </a>
 </template> 
